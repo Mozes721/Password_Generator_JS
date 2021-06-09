135b64f3424b612153ab44b12ac4cb8a40f7eba6
Mozes721 <Richard.Taujenis@gmail.com>
2021-06-09 18:56:21 +0200

    password generator working

--- script.js
+++ script.js
@@ -1,59 +1,42 @@
-// Form
-const form = document.getElementById('passwordGeneratorForm');
-// get all input types 
-
 const characterRange = document.getElementById('slider-range');
 const characterUppercase = document.getElementById('uppercase');
-const characterLowercase = document.getElementById('lowercase');
 const characterSymbols = document.getElementById('symbols');
 const characterNumbers = document.getElementById('numbers');
 const passwordDisplay = document.getElementById("passwordDisplay");
+// copy to clipboard
+const copyClipboard = document.getElementById("CopyToClipBoard");
 
-const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
-const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
-const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
-const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
-  arrayFromLowToHigh(58, 64)
-).concat(
-  arrayFromLowToHigh(91, 96)
-).concat(
-  arrayFromLowToHigh(123, 126)
-)
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*_-+=";
 
-    const characterLenght = characterRange.value;
-    const includeUppercase = characterUppercase.checked;
-    const includeLowercase = characterLowercase.checked;
-    const includeSymbols = characterSymbols.checked;
-    const includeNumbers = characterNumbers.checked;
-    const password = generatePassword(characterLenght, includeUppercase, includeLowercase, includeSymbols, includeNumbers);
-    
+document.getElementById("generate-password").addEventListener("click", e => {
+  e.preventDefault();
+  let characters = lowercase;
+    const characterAmount = characterRange.value;
+    characterUppercase.checked ? (characters += uppercase) : "";
+    characterSymbols.checked ? (characters += symbols) : "";
+    characterNumbers.checked ? (characters += numbers) : "";
+    const password = generatePassword(characterAmount, characters);
+
     passwordDisplay.innerText = password;
-})
+  });
 
-function generatePassword(characterLenght, includeUppercase, includeLowercase, includeSymbols, includeNumbers) {
-    let charPassword = LOWERCASE_CHAR_CODES
-    // if (includeLowercase) charPassword = charPassword.concat(lowercase)
-    if (includeUppercase) charPassword = charPassword.concat(UPPERCASE_CHAR_CODES)
-    if (includeSymbols) charPassword = charPassword.concat(SYMBOL_CHAR_CODES)
-    if (includeNumbers) charPassword = charPassword.concat(NUMBER_CHAR_CODES)
 
-    const passwordCharacters = []
-    for (let i = 0; i < characterLenght; i++) {
-        const caracterCode = charPassword[Math.floor(Math.random() * characterLenght.length)]
-        passwordCharacters.push(String.fromCharCode(caracterCode))
-    
-    // alert(caracterCode);
-    }
-    return passwordCharacters.join('')
-}
-
-function arrayFromLowToHigh(low, high) {
-  const array = []
-  for (let i = low; i <= high; i++) {
-    array.push(i)
+const generatePassword = (lenght, characters) => {
+  let password = "";
+  for (let i = 0; i < lenght; i++) {
+    password += characters.charAt(Math.floor(Math.random() * characters.length)
+    );
   }
-  return array
-}
+  return password;
+};
+
+copyClipboard.addEventListener("click", () => {
+  passwordDisplay.select();
+  document.execCommand("CopyToClipBoard");
+  alert("Password Copied!");
+
+})
